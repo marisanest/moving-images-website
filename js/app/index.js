86e3174c89849c5d5190506ffcad1f7d75cecf1c
marisanest <marisanest@mailbox.org>
2022-01-24 19:40:15 +0100

    Update break points.

---
+++ js/app/index.js
@@ -0,0 +1,93 @@
+const shadersTemplate = {
+    '<>':'div', 'class': 'body-grid-item ${filters}', 'html': [
+        {'<>':'div', 'class': 'image-caption-wrapper', 'html': [
+                {'<>':'div', 'class': 'image-wrapper', 'html': [
+                        {'<>':'a', 'class': 'image-link', 'href':'/app/show.html?data=${filename}.frag', 'html': [
+                                {'<>':'img', 'class': 'image', 'src':"/data/images/${filename}.png", 'alt':"#"}]
+                        }]
+                },
+                {'<>':'div', 'class':'caption small', 'html': [
+                        {'<>':'a', 'href':'/app/show.html?data=${filename}.frag', 'text':'${author} - ${title}'}]
+                }]
+        }]
+};
+
+const menuTemplate = {
+    '<>':'div', 'class': 'body-grid-item grid-item--width2 ${filters}', 'html': [
+        {'<>':'div', 'class': 'image-caption-wrapper', 'html': [
+                {'<>':'div', 'class': 'small', 'html': [
+                        {'<>':'p', 'class': 'medium', 'text':'${title}'}], 'text':'${text}'
+                }]
+        }]
+};
+
+fetch("/data/data.json", {
+    headers: {
+        'Concept-type': 'application/json',
+        'Accept': 'application/json'
+    }
+})
+    .then(resp => resp.json())
+    .then(data => {
+        let bodyGrid = $('.body-grid');
+        bodyGrid.json2html(data["shaders"], shadersTemplate);
+        bodyGrid.json2html(data["menu"], menuTemplate);
+    })
+    .then(_ => {
+        $(window).resize(function() {
+            let cachedWidth = $(window).width();
+            if ($(window).width() != cachedWidth) {
+                $('.collapsible').toggleClass("active", false)
+                $('.filter-menu').css('height', '');
+                $('.filters').css('maxHeight', '');
+            }
+        });
+
+        let headerGrid = $('.header-grid');
+        headerGrid.isotope({
+            itemSelector: '.header-grid-item',
+            masonry: {
+                columnWidth: '.header-grid-sizer',
+                percentPosition: true,
+            }
+        });
+
+        let bodyGrid = $('.body-grid');
+        bodyGrid.isotope({
+            itemSelector: '.body-grid-item',
+            masonry: {
+                columnWidth: '.body-grid-sizer',
+                percentPosition: true,
+            }
+        });
+
+        let filterMenu = $('.filter-menu');
+        filterMenu.on( 'click', '.filter', function() {
+            let filterValue;
+            if ($(this).is('.is-checked')) {
+                filterValue = '*';
+            } else {
+                filterValue = $(this).attr('data-filter');
+                filterMenu.find('.is-checked').removeClass('is-checked');
+            }
+            $(this).toggleClass('is-checked')
+            bodyGrid.isotope({filter: filterValue});
+        });
+
+        $('.collapsible').on("click", function() {
+            currents = $(this);
+            currents.toggleClass("active");
+
+            let parents = currents.closest('.filter-menu');
+            let contents = currents.next();
+
+            if (contents.css('maxHeight') != '0px'){
+                contents.css('maxHeight', '0px');
+                parents.css('height', parents.outerHeight() - contents.outerHeight() + "px");
+            } else {
+                contents.css('maxHeight', contents.prop('scrollHeight') + "px");
+                parents.css('height', parents.outerHeight() + contents.prop('scrollHeight') + "px");
+            }
+            headerGrid.masonry();
+        });
+    });
---
+++ js/app/show.js
@@ -0,0 +1,21 @@
+let params = {};
+location.search.slice(1).split("&").forEach(function(pair) {
+    pair = pair.split("=");
+    params[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+});
+document.title = params.data.split('.')[0].replace('_', " - ").replace(/([a-z])([A-Z])/g, '$1 $2');
+
+if (params['data'] !== null && params['data'].endsWith('.frag')){
+    let srcFile = params['data'];
+    const glslEditor = new GlslEditor('#glsl_editor', {
+        canvas_size: window.innerWidth * 0.5,
+        canvas_draggable: true,
+        theme: 'monokai',
+        multipleBuffers: true,
+        tooltips: true,
+        watchHash: true,
+        fileDrops: true,
+        menu: false,
+    });
+    glslEditor.open("/data/shaders/" + srcFile);
+}
