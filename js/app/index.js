c3764e5aa67989986d67f532a73326e3f3f21d81
marisanest <marisanest@mailbox.org>
2022-01-25 12:00:35 +0100

    Refactor code; Try smaller break point percentage; Extract filtes from JSON; etc..

--- js/app/index.js
+++ js/app/index.js
@@ -1,99 +1,108 @@
-const shadersTemplate = {
-    '<>':'div', 'class': function(){return('body-grid-item ' + this.filters.join(" "))}, 'html': [
-        {'<>':'div', 'class': 'image-caption-wrapper', 'html': [
-                {'<>':'div', 'class': 'image-wrapper', 'html': [
-                        {'<>':'a', 'class': 'image-link', 'href':'/app/show.html?data=${filename}.frag', 'html': [
-                                {'<>':'img', 'class': 'image', 'src':"/data/images/${filename}.png", 'alt':"#"}]
-                        }]
-                },
-                {'<>':'div', 'class':'caption small', 'html': [
-                        {'<>':'a', 'href':'/app/show.html?data=${filename}.frag', 'text':'${author} - ${title}'}]
-                },
+$(document).ready(function() {
+    let image_template = {
+        '<>':'div', 'class': function(){return('body-grid-item ' + this.filters.join(" "))}, 'html': [
+            {'<>':'div', 'class': 'image-caption-wrapper', 'html': [
+                    {'<>':'div', 'class': 'image-wrapper', 'html': [
+                            {
+                                '<>': 'a', 'class': 'image-link', 'href': '/app/show.html?data=${filename}.frag', 'html': [
+                                    {'<>': 'img', 'class': 'image', 'src': "/data/images/${filename}.png"}]
+                            }]
+                    },
+                    {'<>':'div', 'class':'caption small', 'html': [
+                            {'<>':'a', 'href':'/app/show.html?data=${filename}.frag', 'text':'${author} - ${title}'}]
+                    },
 
-                {"<>":"div", "class": "filter-menu overlay", "html":[
-                    {"<>":"a", "obj":function(){return(this.filters);}, "href": "#", "class": "filter overlay", "data-filter": ".${value}", "text": function(){return(this.value.charAt(0).toUpperCase()+this.value.substring(1))}}]
-                }]
-        }]
-};
+                    {"<>":"div", "class": "filters overlay", "html":[
+                            {"<>":"a", "obj":function(){return(this.filters)}, "href": "#", "class": "filter overlay", "data-filter": ".${value}", "text":function(){return this.value.charAt(0).toUpperCase() + this.value.substring(1)}}]
+                    }]
+            }]
+    };
 
-const menuTemplate = {
-    '<>':'div', 'class': 'body-grid-item grid-item--width2 ${filters}', 'html': [
-        {'<>':'div', 'class': 'image-caption-wrapper', 'html': [
-                {'<>':'div', 'class': 'small', 'html': [
-                        {'<>':'p', 'class': 'medium', 'text':'${title}'}], 'text':'${text}'
-                }]
-        }]
-};
+    let text_template = {
+        '<>':'div', 'class': 'body-grid-item grid-item--width2 ${filters}', 'html': [
+            {'<>':'div', 'class': 'image-caption-wrapper', 'html': [
+                    {'<>':'div', 'class': 'small', 'html': [
+                            {'<>':'p', 'class': 'medium', 'text':'${title}'}], 'text':'${text}'
+                    }]
+            }]
+    };
 
-fetch("/data/data.json", {
-    headers: {
-        'Concept-type': 'application/json',
-        'Accept': 'application/json'
+    let filter_template = {
+        "<>":"ul", 'html': [
+            {'<>':'a', "href": "#", 'class':'filter small-medium', 'data-filter':'.${value}', "text":function(){return this.value.charAt(0).toUpperCase() + this.value.substring(1)}
+            }]
     }
-})
-    .then(resp => resp.json())
-    .then(data => {
-        let bodyGrid = $('.body-grid');
-        bodyGrid.json2html(data["shaders"], shadersTemplate);
-        bodyGrid.json2html(data["menu"], menuTemplate);
+
+    let headerGrid = $('.header-grid');
+    let bodyGrid = $('.body-grid');
+    let filterList = $('.filter-list');
+
+    fetch("/data/data.json", {
+        headers: {
+            'Concept-type': 'application/json',
+            'Accept': 'application/json'
+        }
     })
-    .then(_ => {
-        $(window).resize(function() {
-            let cachedWidth = $(window).width();
-            if ($(window).width() != cachedWidth) {
-                $('.collapsible').toggleClass("active", false)
-                $('.filter-menu').css('height', '');
-                $('.filters').css('maxHeight', '');
+        .then(resp => resp.json())
+        .then(data => {
+            bodyGrid.json2html(data["images"], image_template);
+            bodyGrid.json2html(data["texts"], text_template);
+
+            let filters = new Set();
+            for (let image of data["images"]) {
+                for (let filter of image["filters"]) {
+                    filters.add(filter);
+                }
             }
+            filterList.json2html(Array.from(filters).sort(), filter_template);
+
+        })
+        .then(() => {
+            headerGrid.isotope({
+                itemSelector: '.header-grid-item',
+                masonry: {
+                    columnWidth: '.header-grid-sizer',
+                    percentPosition: true,
+                }
+            });
+
+            bodyGrid.isotope({
+                itemSelector: '.body-grid-item',
+                masonry: {
+                    columnWidth: '.body-grid-sizer',
+                    percentPosition: true,
+                }
+            });
+
+            $('.filter').click(function() {
+                let filterValue;
+
+                if ($(this).is('.is-checked')) {
+                    filterValue = '*';
+                    $('.filter.is-checked').removeClass('is-checked');
+                } else {
+                    filterValue = $(this).attr('data-filter');
+                    $('.filter.is-checked').removeClass('is-checked');
+                    $(".filter[data-filter='"+filterValue+"']").addClass('is-checked');
+                }
+
+                bodyGrid.isotope({filter: filterValue});
+            });
+
+            $('.collapsible').click(function() {
+                let collapsible = $(this);
+                collapsible.toggleClass("active");
+                let filters = collapsible.closest('.filters');
+                let filterList = collapsible.next();
+
+                if (filterList.css('maxHeight') != '0px'){
+                    filterList.css('maxHeight', '0px');
+                    filters.css('height', filters.outerHeight() - filterList.outerHeight() + "px");
+                } else {
+                    filterList.css('maxHeight', filterList.prop('scrollHeight') + "px");
+                    filters.css('height', filters.outerHeight() + filterList.prop('scrollHeight') + "px");
+                }
+                headerGrid.masonry();
+            });
         });
-
-        let headerGrid = $('.header-grid');
-        headerGrid.isotope({
-            itemSelector: '.header-grid-item',
-            masonry: {
-                columnWidth: '.header-grid-sizer',
-                percentPosition: true,
-            }
-        });
-
-        let bodyGrid = $('.body-grid');
-        bodyGrid.isotope({
-            itemSelector: '.body-grid-item',
-            masonry: {
-                columnWidth: '.body-grid-sizer',
-                percentPosition: true,
-            }
-        });
-
-        let filterMenu = $('.filter-menu');
-        filterMenu.on( 'click', '.filter', function() {
-            let filterValue;
-            if ($(this).is('.is-checked')) {
-                filterValue = '*';
-                $('.filter-menu .is-checked').removeClass('is-checked');
-                console.log($('.filter-menu .is-checked'))
-            } else {
-                filterValue = $(this).attr('data-filter');
-                $('.filter-menu .is-checked').removeClass('is-checked');
-                $(".filter-menu [data-filter='"+filterValue+"']").addClass('is-checked');
-            }
-            bodyGrid.isotope({filter: filterValue});
-        });
-
-        $('.collapsible').on("click", function() {
-            currents = $(this);
-            currents.toggleClass("active");
-
-            let parents = currents.closest('.filter-menu');
-            let contents = currents.next();
-
-            if (contents.css('maxHeight') != '0px'){
-                contents.css('maxHeight', '0px');
-                parents.css('height', parents.outerHeight() - contents.outerHeight() + "px");
-            } else {
-                contents.css('maxHeight', contents.prop('scrollHeight') + "px");
-                parents.css('height', parents.outerHeight() + contents.prop('scrollHeight') + "px");
-            }
-            headerGrid.masonry();
-        });
-    });
+});
